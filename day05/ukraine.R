c634c7a78d7d1a24393e41ab62e8d752fda0e28f
RamiKrispin <ramkrisp@umich.edu>
2022-11-05 21:40:23 -0700

    update the plot

--- day05/ukraine.R
+++ day05/ukraine.R
@@ -1,7 +1,10 @@
 # 2022 Russian Invasion of Ukraine
+# Data
 # Source: https://github.com/zhukovyuri/VIINA
 # Source: OpenStreetMap
-# Reference: https://ggplot2tutor.com/tutorials/streetmaps
+# Reference:
+# https://ggplot2tutor.com/tutorials/streetmaps
+# http://joshuamccrain.com/tutorials/maps/streets_tutorial.html
 
 df <- readr::read_csv("day05/events_latest.csv")
 
@@ -11,10 +14,12 @@ library(sf)
 library(dplyr)
 library(ggplot2)
 
-city_coords <- getbb("Kiev Ukraine")
-# Events table
-# How to add it to geom_sf???
-kiev_df <- df %>% dplyr::filter(longitude > city_coords[1,1] & longitude > city_coords[1,2],
+city_coords <- matrix(data = c(30.0682, 50.2742, 31.1641, 50.6956),
+                      nrow = 2, ncol = 2)
+
+city_coords <- getbb("Kyiv Ukraine")
+
+kyiv_df <- df %>% dplyr::filter(longitude > city_coords[1,1] & longitude > city_coords[1,2],
                                 latitude > city_coords[2,1] & latitude > city_coords[2,2])
 
 # Pull the data from OpenStreetMap
@@ -35,7 +40,7 @@ streets <- opq(bbox = limits)%>%
   osmdata_sf()
 
 river <- opq(bbox = limits)%>%
-  add_osm_feature(key = "waterway", value = c("canal", "river")) %>%
+  add_osm_feature(key = "waterway", value = c("canal", "river", "riverbank")) %>%
   osmdata_sf()
 
 
@@ -56,18 +61,29 @@ ggplot() +
           color = "#ffbe7f",
           size = .2,
           alpha = .5) +
+  geom_point(data = kyiv_df,
+             mapping = aes(x = longitude, y = latitude),
+             colour = "red") +
   coord_sf(xlim = city_coords[1,],
            ylim = city_coords[2,],
-           expand = FALSE) +
+           expand = TRUE) +
   theme_void() +
-  # labs(title = "Kiev",
-  #      caption = "Data Source: OpenStreetMap") +
+  labs(title = "Kyiv",
+       subtitle = "30.616°N / 50.484°E",
+       caption = "Data Source: OpenStreetMap") +
   theme(
     plot.background = element_rect(fill = "#282828"),
-    plot.title= element_text(size=12,
+    plot.title= element_text(size=12, hjust=.5,
                                color="White",
-                               face="bold")
+                               face="bold"),
+    plot.caption= element_text(size=8,
+                             color="White",
+                             face="plain"),
+    plot.subtitle = element_text(size = 8, hjust=.5,
+                                 color="White",
+                                 margin=margin(2, 0, 5, 0))
   )
 
+ggsave("day5.png", width = 6, height = 6)
 
 
